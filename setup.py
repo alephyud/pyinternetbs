8cd2bf5cf71f49cf1981eaa94f2a02cc1bcb4574
alephyud <alephyud@users.noreply.github.com>
2018-03-30 21:10:01 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 
 from setuptools import setup
 
-setup(name='yandex_pdd',
+setup(name='pyinternetbs',
       version='0.1',
       description='Internetbs Python API',
       url='http://github.com/alephyud/pyinternetbs',
       author='vmax@github',
       license='MIT',
-      packages=['pyinternetbs'],
+      packages=['internetbs'],
       install_requires=[
           'requests',
       ],
